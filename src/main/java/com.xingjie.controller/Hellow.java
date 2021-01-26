39b9509573c6691cffda3e1036ecf69243aaec58
xingjie <xingjiebj@163.com>
2021-01-26 23:36:09 +0800

    init git-01-text-1

--- src/main/java/com.xingjie.controller/Hellow.java
+++ src/main/java/com.xingjie.controller/Hellow.java
@@ -4,5 +4,7 @@ public class Hellow {
     public static void main(String[] args) {
         System.out.printf("Hello Word");
         System.out.printf("sss");
+
+        System.out.printf("abs");
     }
 }
